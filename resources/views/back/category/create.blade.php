2182044b49f1c67c3a570a1a5704b6d443153191
TrishaIbtesham <96471261+TrishaSabrina@users.noreply.github.com>
2023-06-11 15:37:40 +0600

    Category Crud

---
+++ resources/views/back/category/create.blade.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Create Category</title>
+    <style>
+    .form-box {
+        padding: 8px;
+        border-radius: 4px;
+        border: 1px solid skyblue;
+    }
+
+    .head-love {
+        padding: 8px;
+        border-radius: 4px;
+        border: 1px solid skyblue;
+    }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+
+                <div class="head-love">
+
+
+                    <h1>Create Category</h1>
+                </div>
+
+
+                <div class="form-box">
+                    <form action="" method="">
+
+                        <div class="form-group">
+                            <label for="name">Category Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Category Name">
+                        </div><br>
+
+                        <div class="form-group">
+                            <label for="name">Category Details</label>
+                            <input type="text" class="form-control" id="details" name="details"
+                                placeholder="Category Details">
+                        </div><br>
+
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-success" value="submit">
+                        </div>
+
+                    </form>
+
+                </div>
+
+
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
